4c1e4bbe9ba1c939aeeba52c9647c840757194c4
Jakob <jakob.fonstad92@gmail.com>
2020-12-02 10:49:20 +0100

    adjustments

--- src/day2/index.js
+++ src/day2/index.js
@@ -5,12 +5,12 @@ const prepareInput = (rawInput) => rawInput
 const fixInput = (input) => {
   let r = input.split('\n');
   let b = [];
-  r.map(element => {
+  r.map(e => {
     b.push({
-      lower: element.substring(element.lastIndexOf('-'), 0).replace(/[^0-9]{1,2}/, ""),
-      upper: element.substring(element.lastIndexOf('-'), element.lastIndexOf('-')+3).replace(/[^0-9]{1,2}/, ""),
-      letter: element.charAt(element.lastIndexOf(':') - 1),
-      string: element.substring(element.lastIndexOf(':') + 2).replace(/(\r\n|\n|\r)/gm, ""),
+      l: e.substring(e.lastIndexOf('-'), 0).replace(/[^0-9]{1,2}/, ""),
+      u: e.substring(e.lastIndexOf('-'), e.lastIndexOf('-')+3).replace(/[^0-9]{1,2}/, ""),
+      c: e.charAt(e.lastIndexOf(':') - 1),
+      s: e.substring(e.lastIndexOf(':') + 2).replace(/(\r\n|\n|\r)/gm, ""),
     })
   })
   return b;
@@ -20,29 +20,29 @@ let input = prepareInput(readInput());
 
 input = fixInput(input);
 
-const goA = (input) => {
-  let numberCorrect = 0;
-  input.forEach(element => {
-    let letterCount = 0;
-    [...element.string].forEach((e, index) => {
-      if(e === element.letter)
-      letterCount++
+const goA = (i) => {
+  let n = 0;
+  i.forEach(s => {
+    let c = 0;
+    [...s.s].forEach(e => {
+      if(e === s.c)
+        c++
     })
-    if(letterCount >= element.lower && letterCount <= element.upper)
-      numberCorrect++;
+    if(c >= s.l && c <= s.u)
+      n++;
   }) 
-  return numberCorrect;
+  return n;
 }
 
-const goB = (input) => {
-  let numberCorrect = 0;
-  input.forEach(element => {
-    let lower = element.string.charAt(element.lower - 1) === element.letter;
-    let upper = element.string.charAt(element.upper - 1) === element.letter;
-    if((lower || upper) && !(lower && upper)){
-      numberCorrect++;}
+const goB = (i) => {
+  let n = 0;
+  i.forEach(s => {
+    let l = s.s.charAt(s.l - 1) === s.c;
+    let u = s.s.charAt(s.u - 1) === s.c;
+    if((l || u) && !(l && u))
+      n++;
   })
-  return numberCorrect
+  return n
 }
 
 /* Tests */
